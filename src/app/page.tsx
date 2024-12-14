153850f8d56493486b7222f553b0947e8bb941f6
DaimImran <daimimran345@gmail.com>
2024-12-14 19:28:18 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
+
+import Link from "next/link";
 import Hero from "./components/hero";
+import Navbar from "./components/navbar";
+
 
 export default function Home() {
   return (
     <main className="max-w-7xl mx-12 ">
     <Hero />
+    
+
     </main>
   );
 }
